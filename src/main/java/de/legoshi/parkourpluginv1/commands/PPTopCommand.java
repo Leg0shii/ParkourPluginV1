f07876c8cf07b4007be47f763cae5fb1b2c9762d
KingBranBran <kingbran4521@gmail.com>
2020-08-11 14:37:34 -0700

    Fix indent on PPTopCommand.java

--- src/main/java/de/legoshi/parkourpluginv1/commands/PPTopCommand.java
+++ src/main/java/de/legoshi/parkourpluginv1/commands/PPTopCommand.java
@@ -15,77 +15,77 @@ import java.util.function.Consumer;
 
 public class PPTopCommand implements CommandExecutor {
 
-			@Override
-			public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-						AsyncMySQL mySQL = Main.getInstance().mySQL;
-						Main instance = Main.getInstance();
+        AsyncMySQL mySQL = Main.getInstance().mySQL;
+        Main instance = Main.getInstance();
 
-						if (!(sender instanceof Player)) return true;
+        if (!(sender instanceof Player)) return true;
 
-						Player player = ((Player) sender).getPlayer();
+        Player player = ((Player) sender).getPlayer();
 
-						mySQL.query("SELECT ppcountp, playername, playeruuid FROM tablename ORDER BY ppcountp DESC;", new Consumer<ResultSet>() {
+        mySQL.query("SELECT ppcountp, playername, playeruuid FROM tablename ORDER BY ppcountp DESC;", new Consumer<ResultSet>() {
 
-									@Override
-									public void accept(ResultSet resultSet) {
+            @Override
+            public void accept(ResultSet resultSet) {
 
-												try {
+                try {
 
-															int i = 1;
-															int pageAmount = 1;
-															int totalPageAmount = instance.mySQLManager.getPages(resultSet);
+                    int i = 1;
+                    int pageAmount = 1;
+                    int totalPageAmount = instance.mySQLManager.getPages(resultSet);
 
-															if (args.length == 1) {
+                    if (args.length == 1) {
 
-																		int enteredPage = Integer.parseInt(args[0]);
+                        int enteredPage = Integer.parseInt(args[0]);
 
-																		if(enteredPage <= totalPageAmount && enteredPage >= 1) {
+                        if(enteredPage <= totalPageAmount && enteredPage >= 1) {
 
-																					pageAmount = enteredPage;
+                            pageAmount = enteredPage;
 
-																		}
+                        }
 
-																		else {
+                        else {
 
-																					player.sendMessage(Message.ERR_PAGENOTEXIST.getMessage());
-																					return;
+                            player.sendMessage(Message.ERR_PAGENOTEXIST.getMessage());
+                            return;
 
-																		}
+                        }
 
-															}
+                    }
 
-															resultSet.absolute(((pageAmount-1)*10));
+                    resultSet.absolute(((pageAmount-1)*10));
 
-															if (resultSet.next()) {
+                    if (resultSet.next()) {
 
-																		player.sendMessage(Message.MSG_HEADERCOURSECLEAR.getRawMessage());
+                        player.sendMessage(Message.MSG_HEADERCOURSECLEAR.getRawMessage());
 
-																		do {
+                        do {
 
-																					player.sendMessage(Message.MSG_COURSECLEAR.getRawMessage().replace("{num}", Integer.toString(i))
-																							.replace("{player}", resultSet.getString("playername"))
-																							.replace("{ppscore}", Double.toString(resultSet.getDouble("ppcountp"))));
+                            player.sendMessage(Message.MSG_COURSECLEAR.getRawMessage().replace("{num}", Integer.toString(i))
+                                    .replace("{player}", resultSet.getString("playername"))
+                                    .replace("{ppscore}", Double.toString(resultSet.getDouble("ppcountp"))));
 
-																					i++;
+                            i++;
 
-																		} while (i < 11 && resultSet.next());
+                        } while (i < 11 && resultSet.next());
 
-																		player.sendMessage(Message.MSG_FOOTERCOURSECLEAR.getRawMessage());
-																		player.sendMessage(Message.MSG_PAGEAMOUNT.getRawMessage()
-																				.replace("{page}", Integer.toString(pageAmount))
-																				.replace("{pagetotal}", Integer.toString(totalPageAmount)));
+                        player.sendMessage(Message.MSG_FOOTERCOURSECLEAR.getRawMessage());
+                        player.sendMessage(Message.MSG_PAGEAMOUNT.getRawMessage()
+                                .replace("{page}", Integer.toString(pageAmount))
+                                .replace("{pagetotal}", Integer.toString(totalPageAmount)));
 
-															}
+                    }
 
-												} catch (SQLException throwables) { throwables.printStackTrace(); }
+                } catch (SQLException throwables) { throwables.printStackTrace(); }
 
-									}
+            }
 
-						});
+        });
 
-						return false;
+        return false;
 
-			}
+    }
 
 }
